cfc01ebcccaf18deb27eae3a397c4c760772941f
Sungsu-Kim <sungsukim0609@gmail.com>
2023-05-21 01:31:04 +0900

    tutorial of changing github

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
-print("hello")
-print("hello python")
-int(input("숫자입력:"))
+print("visual에서 수정 후 github에 수정본 보내는 법")
+print("1. 수정 파일 visual에 있는 터미널에 git add . 작성.")
+print("2. git status 로 수정된 파일 확인.")
+print("3. git commit -m "second commit"  작성.")
+print("(여기서 큰 따옴표 안에는 히스토리 이름을 작성 ex> 세번 째 파일일 경우: third commit)")
